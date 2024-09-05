41dcfb9f71cad8f7b75d88bca339cecd0f74a3f1
Makai <makai@mk.mk>
2024-09-05 11:25:51 -0300

    .

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,13 @@
 use gtk::prelude::*;
 use gtk::{glib, Application};
 mod main_window;
+mod server_logic;
 
 const APP_ID: &str = "org.gtk_rs.HelloWorld2";
 
 fn main() -> glib::ExitCode 
 {
-    // Create a new application
     let app = Application::builder().application_id(APP_ID).build();
-
-    // Connect to "activate" signal of `app`
     app.connect_activate(main_window::build_ui);
-
-    // Run the application
     app.run()
 }
--- src/main_window.rs
+++ src/main_window.rs
@@ -1,16 +1,27 @@
-use gtk::prelude::*;
+use gtk::{prelude::*, Button};
 use gtk::{ApplicationWindow, Application};
+
+use crate::server_logic;
 
 pub fn build_ui(app: &Application) 
 {
-    // Create a window and set the title
-    let window = ApplicationWindow::builder()
+    let button_initiate_server = Button::builder()
+        .label("Iniciar servidor")
+        .margin_bottom(500)
+        .build();
+
+    let main_window = ApplicationWindow::builder()
         .application(app)
         .title("Cube")
         .height_request(640)
         .width_request(960)
+        .child(&button_initiate_server)
         .build();
 
-    // Present window
-    window.present();
+    button_initiate_server.connect_clicked(|_|
+    {
+        server_logic::init_server();
+    });
+
+    main_window.present();
 }
