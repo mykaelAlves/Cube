9f1084f01d5553ba6adb2c0d9a9bc06f8c90529b
Mykael A. <mykaelalvesnogueira1@gmail.com>
2024-10-15 16:14:08 -0300

    .

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,4 @@
-use iced::widget::text;
-use iced::{Sandbox, Settings};
-
-fn main() -> iced::Result
+fn main()
 {
-    CubeApp::run(Settings::default())
-}
-
-struct CubeApp;
-
-#[derive(Debug)]
-enum Message {}
-
-impl Sandbox for CubeApp
-{
-    type Message = Message;
-
-    fn new() -> Self
-    {
-        Self 
-    }
-
-    fn title(&self) -> String 
-    {
-        String::from("Cube")
-    }
-
-    fn update(&mut self, message: Self::Message) 
-    {
-        match message 
-        {
-
-        }
-    }
-
-    fn view(&self) -> iced::Element<'_, Self::Message> 
-    {
-        text("Testando...").into()
-    }
+    
 }
